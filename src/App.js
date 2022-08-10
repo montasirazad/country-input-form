767b13b35d58803222d63ae0d16378962401bd7d
montasirazad <montasirazad@gmail.com>
2022-08-10 19:03:06 +0600

    found data

--- src/App.js
+++ src/App.js
@@ -1,49 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [allCountry, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://restcountries.com/v2/all'
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setAllCountries(data)
+      })
+  }, [])
+
   return (
     <div className="App">
       <h3>Country input form</h3>
-      <div className="input-group mb-3">
-        <label className="input-group-text" htmlFor="inputGroupSelect01">Options</label>
-        <select className="form-select" id="inputGroupSelect01">
-          <option selected>Choose...</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+
+      <div className="input-group mb-3 w-50" >
+
+        <select className="form-select" id="inputGroupSelect01" defaultValue>
+          <option value="">Select country</option>
+          {
+            allCountry.map(country => <option key={country.numericCode} value={country.name}>{country.name}</option>)
+          }
+
         </select>
       </div>
 
-      <div className="input-group mb-3">
-        <select className="form-select" id="inputGroupSelect02">
-          <option selected>Choose...</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <label className="input-group-text" htmlFor="inputGroupSelect02">Options</label>
-      </div>
-
-      <div className="input-group mb-3">
-        <button className="btn btn-outline-secondary" type="button">Button</button>
-        <select className="form-select" id="inputGroupSelect03" aria-label="Example select with button addon">
-          <option selected>Choose...</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-      </div>
-
-      <div className="input-group">
-        <select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-          <option selected>Choose...</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <button className="btn btn-outline-secondary" type="button">Button</button>
-      </div>
     </div>
   );
 }
